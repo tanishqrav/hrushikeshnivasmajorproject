e37f47fdf33a772a0255d3a1e5c59396863fe9a5
tanishqrav <159329533+tanishqrav@users.noreply.github.com>
2024-03-14 21:53:25 +0530

    Update web_functions.py

--- web_functions.py
+++ web_functions.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 import streamlit as st
 
 
@@ -31,17 +33,23 @@ def train_model(X, y):
             min_samples_split=2, min_weight_fraction_leaf=0.0,
             random_state=42, splitter='best'
         )
+    model1=RandomForestClassifier(n_estimators=100,random_state=42)
+    model2=SVC(kernel='linear',random_state=42)
     # Fit the data on model
     model.fit(X, y)
+    model1.fit(X,y)
+    model2.fit(X,y)
     # Get the model score
     score = model.score(X, y)
+    score1=model.score(X,y)
+    score2=model.score(X,y)
 
     # Return the values
-    return model, score
+    return model, score,model1,score1,model2,score2
 
 def predict(X, y, features):
     # Get model and model score
-    model, score = train_model(X, y)
+    model, score,model1,score1,model2,score2 = train_model(X, y)
     # Predict the value
     prediction = model.predict(np.array(features).reshape(1, -1))
 
